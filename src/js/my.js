c0b9b0d655cd18491e8e46c695856835647d500e
sunnycharkhwal <sunny.charkhwal@gmail.com>
2022-09-19 15:47:27 +0530

    Add files via upload
    1

---
+++ src/js/my.js
@@ -0,0 +1,362 @@
+$(function () {
+  $(".carosele").slick({
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 2000,
+    dots: false,
+    arrows: true,
+    prevArrow: $(".carosel-nav-lefts"),
+    nextArrow: $(".carosel-nav-rights"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: false,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: false,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+    ],
+  });
+});
+$(function () {
+  $(".caroselee").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 2000,
+    dots: false,
+    arrows: true,
+    prevArrow: $(".carosel-nav-leftee"),
+    nextArrow: $(".carosel-nav-rightee"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+    ],
+  });
+});
+$(function () {
+  $(".carosels").slick({
+    infinite: false,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 2000,
+    dots: false,
+    arrows: true,
+    prevArrow: $(".carosel-nav-left"),
+    nextArrow: $(".carosel-nav-right"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+    ],
+  });
+});
+$(function () {
+  $(".caroseleee").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 2000,
+    dots: false,
+    arrows: true,
+    prevArrow: $(".carosel-nav-leftee"),
+    nextArrow: $(".carosel-nav-rightee"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+    ],
+  });
+});
+$(function () {
+  $(".caroselm").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 2000,
+    dots: false,
+    arrows: true,
+    prevArrow: $(".carosel-nav-leftm"),
+    nextArrow: $(".carosel-nav-rightm"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          autoplayspeed: 2000,
+          infinite: true,
+          dots: false,
+          infinite: true,
+        },
+      },
+    ],
+  });
+});
+
+const allHoverImages = document.querySelectorAll(".hover-container div img");
+const imgContainer = document.querySelector(".img-container");
+
+window.addEventListener("DOMContentLoaded", () => {
+  allHoverImages[0].parentElement.classList.add("active");
+});
+
+allHoverImages.forEach((image) => {
+  image.addEventListener("mouseover", () => {
+    imgContainer.querySelector("img").src = image.src;
+    resetActiveImg();
+    image.parentElement.classList.add("active");
+  });
+});
+
+function resetActiveImg() {
+  allHoverImages.forEach((img) => {
+    img.parentElement.classList.remove("active");
+  });
+}
+// sel
